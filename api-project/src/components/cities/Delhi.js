1137b3c6e65ab4b59c1a464a52c065a2ee4a3012
Oliver Pettyfer <oliverpettyfer@Olivers-MacBook-Pro.local>
2021-08-29 17:21:16 +0100

    ollys second update

---
+++ api-project/src/components/cities/Delhi.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Delhi = () => {
+  return <h1>Delhi Page</h1>;
+};
+
+export default Delhi;
---
+++ api-project/src/components/cities/London.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const London = () => {
+  return <h1>London Page</h1>;
+};
+
+export default London;
