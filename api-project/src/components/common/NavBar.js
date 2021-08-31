34931f33893efe6e87f9f4f7d2ac07fe306765c2
Oliver Pettyfer <oliverpettyfer@Olivers-MacBook-Pro.local>
2021-08-31 17:05:15 +0100

    updates with bulma

--- api-project/src/components/common/NavBar.js
+++ api-project/src/components/common/NavBar.js
@@ -1,7 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <h1>NavBar</h1>;
+  return (
+    <nav>
+      <div className="navbar is-light">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              Home
+            </Link>
+            <Link to="/london" className="navbar-item">
+              London
+            </Link>
+            <Link to="/newyork" className="navbar-item">
+              New York
+            </Link>
+            <Link to="/delhi" className="navbar-item">
+              Delhi
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
+
+/* <nav>
+      <div className="navbar is-light">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              Home
+            </Link>
+            <Link to="/wines" className="navbar-item">
+              Wine List
+            </Link>
+            <Link to="/wines/add" className="navbar-item">
+              Add Wine
+            </Link>
+            <Link to="/register" className="navbar-item">
+              Register
+            </Link>
+            <Link to="/login" className="navbar-item">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav> */
